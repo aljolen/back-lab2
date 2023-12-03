d462a12fde4e7ae4ab7bc1e4294e5b1954fabcd4
Alina Barynina <alina00427@gmail.com^>
2023-12-03 02:20:52 +0100

    Add filtering record endpoint

--- webapp/records.py
+++ webapp/records.py
@@ -12,6 +12,18 @@ def get_record(id):
     return dataclasses.asdict(records[id])
 
 
+@app.get("/record")
+def get_record_filter():
+    user_id = request.args.get("user_id")
+    category_id = request.args.get("category_id")
+    if user_id is None and category_id is None:
+        return {
+            "error": "At least one of the following query parameters should be present: [user_id, category_id]"}, 404
+    return list(filter(lambda r: category_id is None or r.category_id == category_id,
+                       filter(lambda r: user_id is None or r.user_id == user_id,
+                              records.values())))
+
+
 @app.delete("/record/<id>")
 def delete_record(id):
     return dataclasses.asdict(records.pop(id))
