273f38a06162ebebe4f4a08e23235ed572a77513
Alina Barynina <alina00427@gmail.com>
2023-12-21 11:26:49 +0100

    Extract config as environmental variables

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
 
 FLASK_DEBUG = True
-SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:123456@localhost:5432/postgres"
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASSWORD']
+db_host = os.environ['DB_HOST']
+db_name = os.environ['DB_NAME']
+SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
